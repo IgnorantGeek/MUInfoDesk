2897b08ff5e4000b52f4a8e4e565492c1ba172d9
Heisler <memu_nheisler@iastate.edu>
2019-02-03 17:16:40 -0600

    changed some link titles to be less misleading

--- application/views/backend/view-presets.php
+++ application/views/backend/view-presets.php
@@ -19,7 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <div class ="topnav">
         <a class="active" href="<?php echo base_url()?>Main/backendhome">Home</a>
-        <a class="active" href="<?php echo base_url()?>Main/viewalldatabase">Previous Page</a>
+        <a class="active" href="<?php echo base_url()?>Main/viewalldatabase">All Databases</a>
         <a href="<?php echo base_url()?>Databaseview/logout">Logout</a>
     </div>
 
